c2c8855810047c5db08c22d4fa9490bbeb6d591d
Sam Kituka <s.kituka1@gmail.com>
2022-05-19 13:10:32 -0400

    Add project files.

---
+++ SoccerPlayer.UI.CRUDConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Data;
+using SoccerPlayer.DataAcess.Controllers;
+using SoccerPlayer.DataAcess.Models;
+
+namespace SoccerPlayer.UI.CRUDConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayerController playerController = new PlayerController();
+            playerController.CreatePlayer("Thomas", "Muller", "32", "Bayern Munich", " Forward");
+           
+        }
+    }
+}
